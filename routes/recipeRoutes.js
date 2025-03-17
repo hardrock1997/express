2dd713e01c3af5d459ea3c24c3933687eb2f7bb3
vicfirth@1997 <yashmundra1997@gmail.com>
2025-03-17 13:31:59 +0530

    single recipe get api implementation

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -2,10 +2,12 @@ const express = require("express")
 const getAllRecipes = require("../controllers/recipeController")
 const postController = require("../controllers/postController")
 const deleteController = require("../controllers/deleteController")
+const singleRecipeController = require("../controllers/singleRecipeController")
 
 const recipeRouter  =  express.Router()
 
 
 recipeRouter.route("/").get(getAllRecipes).post(postController).delete(deleteController)
+recipeRouter.route("/:recipeId").get(singleRecipeController)
 
 module.exports = recipeRouter
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const morgan = require("morgan")
 
 const recipeRouter = require("./routes/recipeRoutes")
 
 const app = express();
 
 app.use(express.json());
+
+app.use(morgan("dev"))
 
 app.use("/recipes",recipeRouter)
 
